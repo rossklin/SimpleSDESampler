b586df5768c57f1ec27d2e10092902355b437164
ross <rossklin@gmail.com>
2014-03-19 21:33:16 +0100

    Updated synthetic.dataset.R with documentation

--- R/synthetic.dataset.R
+++ R/synthetic.dataset.R
@@ -1,7 +1,58 @@
+## Software License Agreement (BSD License)
+##
+## Copyright (c) 2014, Ross Linscott (rossklin@gmail.com)
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##
+##     Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##
+##     Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##
+##     The names of its contributors may not be used to endorse or promote products
+##     derived from this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#' Generate Synthetic Dataset
+#'
+#' Simulate sample trajectories of an SDE and store the results in a time.table
+#'
+#' @param num.entities Number of sample trajectories to generate.
+#' @param tmax End of the time interval to integrate the SDE over.
+#' @param steps Number of time steps to use in the integration.
+#' @param process.noise.sd Standard deviation of the brownian motion component.
+#' @param observation.noise.sd Standard deviation of synthetic observation noise.
+#' @param do.standardise Standardise the output?
+#' @param initial.generator Function that takes an index and generates a starting point for a sample SDE trajectory.
+#' @param det.deriv Function computing the deterministic component of the time derivative given the state.
+#' @param stoch.deriv Function computing the stochastic component of the time derivative given the state.
+#' @param jacobian Function computing the jacobian of det.deriv with respect to the state variables.
+#' @param at.times Sequence of times to include in the output.
+#'
+#' Produces sample trajectories for an SDE on Ito form: 
+#' dx(t) = f(x(t)) dt + g(x(t), t) e(t) sqrt(dt)
+#' where det.deriv is f and stoch.deriv is (at the moment) g * e
+#' (probably will change so stoch.deriv is only g in the future)
+#' @export
 synthetic.dataset <- function( num.entities = 10
                               , tmax = 10
-                              , steps = 1000 * tmax
+                              , steps = 40 * tmax
                               , process.noise.sd = 0
                               , observation.noise.sd = .01
                               , do.standardise = F
