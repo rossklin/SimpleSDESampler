69d4a14026daff06910b26e96a4c256acd56e5c1
Ross Linscott <rossklin@gmail.com>
2016-08-25 13:57:55 +0200

    Added --without-matlab for nlopt since the configure seems to break on finding mex installation dir.

--- src/install_nlopt.sh
+++ src/install_nlopt.sh
@@ -3,7 +3,7 @@ echo "running install nlopt..."
 cd ..
 tar xvzf nlopt-2.4.1.tar.gz
 cd nlopt-2.4.1
-./configure --prefix=$PWD --enable-shared
+./configure --prefix=$PWD --enable-shared --without-matlab
 make
 make install
 cp include/* ../inst/include/
